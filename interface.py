1598c9b52cf1a18414e690187be5b07a96e7e515
IVHM <hmatthyseniv@gmail.com>
2019-07-25 17:54:24 -0500

    Add: mockup for data overview window

--- interface.py
+++ interface.py
@@ -18,18 +18,26 @@ logo_in = 'images/temp_logo.gif'
 def do_nothing():
     pass
 
-def just_a_bunch_o_buttons_n_stuff():
-    app.addLabel("test label", "Well look who it is.")   
-    app.addLabel("testes label", "bees")    
-    app.addButtons(["btn1","btn2"], do_nothing )
-    app.addEntry("Bees?")
-    app.addLabels("Bees","Bees")
-    app.startFrame('video killed the radio button')
-    app.addRadioButton("bees", "Yes")
-    app.addRadioButton("bees", "Most definitely")
-    app.addRadioButton("bees", "WHAt??")
-    app.addRadioButton("bees", "BEEEEEES!!!!")
-    app.stopFrame()
+def just_a_bunch_o_buttons_n_stuff(bees):
+    
+    if bees == 1:
+        app.addLabel("test label", "Well look who it is.")   
+        app.addLabel("testes label", "bees")    
+        app.addButtons(["btn1","btn2"], do_nothing )
+    
+    if bees == 2:
+        app.addEntry("Bees?")
+        app.addLabel("Beeeees","Bees")
+   
+    if bees == 3:
+        app.startFrame('video killed the radio button')
+        app.addRadioButton("bees", "Yes")
+        app.addRadioButton("bees", "Most definitely")
+        app.addRadioButton("bees", "WHAt??")
+        app.addRadioButton("bees", "BEEEEEES!!!!")
+        app.stopFrame()
+
+
 
 
 
@@ -45,13 +53,18 @@ def initialize_dataset():
     file_path = app.getEntry("load file")
     if file_path !=  "- Choose a file -": # default
         print("intializing contents of file: ",app.getEntry("load file"))      
-        app.setMeter('generation progress',100)
+        app.setMeter('initialization progress',100)
+        display_window("Main Screen", False)
+        app.destroySubWindow("Main Screen")
         load_data_win()
+        app.show()
     else:
         print("Please choose a file before loading ")
     
     
-    
+def exit_main():
+    app.destroySubWindow("Main Screen")
+    app.stop()    
     
     
 # MAIN WINDOW
@@ -64,27 +77,51 @@ def load_main_win():
     app.stopFrame()
     
     # LOAD FILE
-    app.addOpenEntry("load file",)
+    app.addFileEntry("load file",)
     app.setEntryDefault("load file", "- Choose a file -")
     app.addButton("Generate", initialize_dataset)
     
     # INIT/EXIT BTNS
     app.addMeter("initialization progress")
     app.setMeterFill("initialization progress", "light blue")
-    app.addButton("Exit", app.stop, app.getRow(),3)
+    app.addButton("Exit", exit_main, app.getRow(),3)
     
     app.stopSubWindow()
     
-
 
     
     
 # DATA OVERVIEW/NAVIGATION WINDOW    
 def load_data_win():
-    app.startSubWindow("data overview","900x600")
-    just_a_bunch_o_buttons_n_stuff()
-    app.stopSubWindow()
+    app.startTabbedFrame("Data Win Tabs")
     
+    app.startTab("Overview")
+    just_a_bunch_o_buttons_n_stuff(1)
+    app.stopTab()
+    
+    app.startTab("States")
+    just_a_bunch_o_buttons_n_stuff(2)
+    app.stopTab()
+    
+    app.startTab("Category")
+    just_a_bunch_o_buttons_n_stuff(3)
+    app.stopTab()
+
+    app.stopTabbedFrame()
+    app.setTabbedFrameTabExpand("Data Win Tabs", expand=True)
+
+
+
+
+
+def display_window(window_name, hidden=False):
+    if not hidden:
+        app.showSubWindow(window_name)
+    else:
+        app.hideSubWindow(window_name)        
+    
+
+
     
     
 # Create gui object to hold windows
@@ -92,6 +129,5 @@ app = gui("Subsidy Calculator","900x600")
 
 load_main_win()
 
-
-app.go()
+app.go(startWindow="Main Screen")
 
